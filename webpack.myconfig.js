6ca42c901baf7bb64734227f4945bd896b040768
zhedream <l19517863@163.com>
2019-11-17 17:21:19 +0800

    watch 打包

--- webpack.myconfig.js
+++ webpack.myconfig.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode:'production',
-    entry: './src/index.js',
+    mode: 'production', // 打包模式
+    watch: false, // 开启监视文件,自动打包
+    entry: './src/index.js', // 入口
     output: {
+        // 打包出口
         path: path.resolve(__dirname, 'dist'), // 解析成绝对路径
         filename: 'bundle.js'
     }
